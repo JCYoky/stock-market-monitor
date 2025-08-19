8bc9b5f09e85e40c2c1f24560e981abeb37f22cb
huangjiacheng <616663713@qq.com>
2025-08-19 18:00:36 +0800

    修复定时任务未启动问题：添加@EnableScheduling注解

--- src/main/java/me/huangjiacheng/hundun/HundunApplication.java
+++ src/main/java/me/huangjiacheng/hundun/HundunApplication.java
@@ -2,8 +2,10 @@ package me.huangjiacheng.hundun;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class HundunApplication {
     public static void main(String[] args) {
         SpringApplication.run(HundunApplication.class, args);
